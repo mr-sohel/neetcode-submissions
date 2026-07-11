971a86c028a63507cb925475e644dccc0b523536
Md. Sohel Rana <mrsohelcse@gmail.com>
2026-07-12 01:00:54 +0600

    Add: min-cost-climbing-stairs - submission-1

---
+++ Data Structures & Algorithms/min-cost-climbing-stairs/submission-1.cs
@@ -0,0 +1,16 @@
+public class Solution {
+    int[] dp;
+    public int f(int i, ref int[] cost) {
+        if(i >= cost.Length) {
+            return 0;
+        }
+        if(dp[i] != -1) return dp[i];
+       
+        return dp[i] = cost[i] + Math.Min(f(i+1, ref cost), f(i+2, ref cost));
+    }
+    public int MinCostClimbingStairs(int[] cost) {
+        dp = new int[cost.Length];
+        Array.Fill(dp, -1);
+        return Math.Min(f(0, ref cost), f(1, ref cost));
+    }
+}
